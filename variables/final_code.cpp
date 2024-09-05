cd4a8f90d7ad8613600a7b13c3225efed187cc35
Lalit Saini <codedev@lalitsaini.com>
2024-09-05 22:48:31 +0530

    How to use variables in C++ | Take User input in C++ | single line comment

---
+++ variables/final_code.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    // number = 36;
+    cout << "Please input a number: \n";
+    cin >> number;
+    cout << "The value of number is:" << number << "\n";
+    return 0;
+}
